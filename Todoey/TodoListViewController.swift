73c9453cd4c03ef5dcfd3e5ab5e75df842c29658
Joseph Wysosky <wysoskyj@gmail.com>
2019-11-06 12:53:16 -0500

    Persist added items in user defaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Dogs", "Cats", "Iguanas"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        itemArray = defaults.stringArray(forKey: "TodoListArray") ?? []
     }
 
     //MARK: - TableView Datasource Methods
@@ -51,6 +51,7 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
 
